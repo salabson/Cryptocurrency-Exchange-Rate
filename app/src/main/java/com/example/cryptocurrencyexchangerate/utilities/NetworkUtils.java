8dc40cbc03f7a077aa5d7b51d1af2b02e64f222b
Salabson <salabson4@yahoo.co.uk>
2017-10-23 12:15:45 +0100

    create complete urls by combining base url and query parameters

--- app/src/main/java/com/example/cryptocurrencyexchangerate/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/cryptocurrencyexchangerate/utilities/NetworkUtils.java
@@ -1,5 +1,11 @@
 package com.example.cryptocurrencyexchangerate.utilities;
 
+import android.net.Uri;
+import android.util.Log;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by salabs on 23/10/2017.
  */
@@ -9,4 +15,22 @@ public class NetworkUtils {
     final static String CRYPTOCOMPARE_BASE_URL = "https://min-api.cryptocompare.com/data/price";
     final  static String PARAM_FSYM ="fsym";
     final  static String PARAM_TSYM ="tsyms";
+
+    // create complete urls by combining base url and query parameters
+    private static URL buildURL(String baseCurrency, String otherCurrency ) {
+        Uri builtUri = Uri.parse(CRYPTOCOMPARE_BASE_URL).buildUpon()
+                .appendQueryParameter(PARAM_FSYM,baseCurrency)
+                .appendQueryParameter(PARAM_TSYM,otherCurrency)
+                .build();
+
+        URL url = null;
+
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+        }
+
+        return url;
+    }
+
 }
