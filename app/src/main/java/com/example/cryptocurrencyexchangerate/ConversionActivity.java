b3cc7b8f36c9bb76df2eab5f1c4543b7926a3be2
Salabson <salabson4@yahoo.co.uk>
2017-10-24 21:22:39 +0100

    Define and  initialize UI component in conversion activity

--- app/src/main/java/com/example/cryptocurrencyexchangerate/ConversionActivity.java
+++ app/src/main/java/com/example/cryptocurrencyexchangerate/ConversionActivity.java
@@ -2,21 +2,47 @@ package com.example.cryptocurrencyexchangerate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class ConversionActivity extends AppCompatActivity {
+    String fiatCode;
+    String cryptoSelectedItem;
+    TextView tv_btc_rate;
+    EditText et_coin_amount;
+    TextView tv_total_amount;
+    TextView tv_main_title;
+    TextView tv_sub_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversion);
-        Bundle top = getIntent().getExtras();
-        if (top != null) {
-            String topText = top.getString("top");
-            TextView result = (TextView)findViewById(R.id.tv_result);
-            result.setText(topText);
+        // retrive the bundles from the intent
+        Bundle extras = getIntent().getExtras();
+        if (extras == null) {
+          return;
         }
+        String fiatCode = extras.getString("fiat_code");
+        String cryptoCode = extras.getString("cryto_code");
+
+        // build the main title of the activity
+        tv_main_title = (TextView)findViewById(R.id.tv_main_title);
+        tv_main_title.setText("Convert " + cryptoCode + "to " + fiatCode);
+
+        // build the main title of the activity
+        tv_sub_title = (TextView)findViewById(R.id.tv_sub_title);
+        tv_main_title.setText("Enter amount in " + cryptoCode);
+
+        et_coin_amount = (EditText) findViewById(R.id.et_coin_amount);
+
+        tv_total_amount = (TextView) findViewById(R.id.tv_total_amount);
+        tv_total_amount.setText(fiatCode);
+        tv_btc_rate = (TextView) findViewById(R.id.tv_btc_rate);
+        tv_btc_rate.setText(cryptoCode);
+
+
     }
 }
