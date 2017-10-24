2d1097af354e1977ef013e6b4628eb105c3ac174
Salabson <salabson4@yahoo.co.uk>
2017-10-24 12:31:16 +0100

    Check for internet connectivity

--- app/src/main/java/com/example/cryptocurrencyexchangerate/utilities/GeneralUtils.java
+++ app/src/main/java/com/example/cryptocurrencyexchangerate/utilities/GeneralUtils.java
@@ -1,8 +1,22 @@
 package com.example.cryptocurrencyexchangerate.utilities;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 /**
  * Created by salabs on 24/10/2017.
  */
 
 public class GeneralUtils {
+    // this method check for internet connectivity and return true or false depending on the state
+    public static boolean isInternetConnected(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnectedOrConnecting()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
